60f076a9563c17f00fb4998716d38729d01bac0f
thonc-2135 <nguyen.chinh.tho@sun-asterisk.com>
2025-04-22 10:09:49 +0700

    Fix comment 3

--- src/main/java/com/example/cecv_e_commerce/controller/admin/UserController.java
+++ src/main/java/com/example/cecv_e_commerce/controller/admin/UserController.java
@@ -10,12 +10,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin/users")
-@PreAuthorize("hasRole('ADMIN')")
 public class UserController extends AdminController{
 
     @Autowired
